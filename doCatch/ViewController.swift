2dd9ae7edc33769859b5dd8775a5154ee4ad29f2
Milos Dimic <milos.dimic@htecgroup.com>
2018-10-08 15:52:42 +0200

    implementation

--- doCatch/ViewController.swift
+++ doCatch/ViewController.swift
@@ -10,11 +10,78 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    enum ThrowableError: Error {
+        case somethingBadHappened
+    }
+    
+    // MARK: Properties
+    
+    private var isEverythingOk = true
+    
+    // MARL: View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        takeResponsibilityOfHandlingErrors()
+        try? passTheErrorAlong()
+        appWillCrashIfError()
+        dontCareAboutErrorJustWantResultIfSuccess()
     }
-
-
+    
+    // MARK: Private methods
+    
+    // MARK: Throwing methods
+    
+    private func doSomething() throws -> String {
+        if isEverythingOk {
+            return "Everything is OK"
+        } else {
+            throw ThrowableError.somethingBadHappened
+        }
+    }
+    
+    // MARK: Try
+    
+    private func takeResponsibilityOfHandlingErrors() {
+        do {
+            let _ = try doSomething()
+        }
+        catch {
+            // Here you know about the error
+            // Feel free to handle to re-throw
+            let _ = error
+        }
+    }
+    
+    private func passTheErrorAlong() throws -> Void {
+        // Not within a do-catch block.
+        // Any errors will be re-thrown to callers.
+        let _ = try doSomething()
+    }
+    
+    // MARK: Try!
+    
+    private func appWillCrashIfError() {
+        //  “do or die” situation
+        let _ = try! doSomething()
+    }
+    
+    // MARK: Try?
+    
+    private func dontCareAboutErrorJustWantResultIfSuccess() {
+        if let _ = try? doSomething() {
+            // doSomething succeeded, and result is unwrapped.
+        } else {
+            // doSomething() threw an error but we dont care about error type
+        }
+        
+        // or the same thing coded with guard
+        
+        guard let _ = try? doSomething() else {
+            // doSomething() threw an error but we dont care about error type
+            return
+        }
+    }
 }
 
